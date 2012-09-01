5a3bd6b207da9f17093be773a69d478c0c1b6106
ztbrown015 <zack.t.brown@gmail.com>
2012-08-31 21:36:17 -0400

    configured port for production

--- socket_server.js
+++ socket_server.js
@@ -3,8 +3,9 @@ var express = require('express');
 var app = express.createServer()
 var io = require('socket.io').listen(app);
 
+var port = process.env.PORT || 8080;
 
-app.listen(8080);
+app.listen(port);
 
 app.on('error', function(err) {
     console.log('there was an error: ' + err.message); 
